44282c0bb63cbda5c7c74b79fd2476dfe3373d57
Avinash Dewangan <avinash.dew9@gmail.com>
2021-07-24 01:08:29 +0530

    Day 1 - Express server added

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+
+app.get('/', (req, res) => {
+    return res.send("Hello World!!");
+})
+
+
+app.listen(3000, () => {
+    console.log("Server running at port 3000")
+})
