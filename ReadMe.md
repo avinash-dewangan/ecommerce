857e9a4415f94ad7ac0853a6c224b1788c656a72
Avinash Dewangan <avinash.dew9@gmail.com>
2021-07-24 02:51:15 +0530

    Day_4 added testing dependency and boilerplat using chai and mocha

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,7 @@
 npx eslint --init
+
+testing
+chai and mocha
+search on google 
+express chai http
+https://medium.com/@asciidev/testing-a-node-express-application-with-mocha-chai-9592d41c0083
--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('Hello, World!!')
+  try {
+    // throw new Error('foo')
+    return res.send('Hello, World!!')
+  } catch (error) {
+    res.status(500).send('Internal Server Error')
+  }
 })
 
 app.listen(3000, () => {
   console.log('Server running at port 3000')
 })
+
+module.exports = app
