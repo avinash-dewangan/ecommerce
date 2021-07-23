3b5d2bc54c9ea3889a8847dec1e64f399b18a277
Avinash Dewangan <avinash.dew9@gmail.com>
2021-07-24 01:58:38 +0530

    Day_3 added eslint for code formating and style enforce

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
-
 app.get('/', (req, res) => {
-    return res.send("Hello, World!!");
+  return res.send('Hello, World!!')
 })
 
-
 app.listen(3000, () => {
-    console.log("Server running at port 3000")
+  console.log('Server running at port 3000')
 })
